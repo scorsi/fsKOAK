6630dabb6119d722e729045e76db209f874b2e2a
scorsi <sylvain.corsini@gmail.com>
2018-02-25 00:17:59 +0100

    Better return code (0: Success, 1: Failure)

--- FsKOAK/Program.fs
+++ FsKOAK/Program.fs
@@ -6,10 +6,18 @@ open System.IO
 
 [<EntryPoint>]
 let main argv = 
-    match Lexer.lex (File.ReadAllText argv.[0]) with
-    | Lexer.Success tokens -> 
-        match Parser.parse tokens with
-        | Parser.Success nodes -> printfn "%A" nodes
-        | Parser.Failure msg -> printfn "Error: %s" msg
-    | Lexer.Failure msg -> printfn "Error: %s" msg
-    0
+    let ret = 
+        match Lexer.lex (File.ReadAllText argv.[0]) with
+        | Lexer.Success tokens -> 
+            match Parser.parse tokens with
+            | Parser.Success nodes -> 
+                printfn "%A" nodes
+                true
+            | Parser.Failure msg -> 
+                printfn "Error: %s" msg
+                false
+        | Lexer.Failure msg -> 
+            printfn "Error: %s" msg
+            false
+    if ret = true then 0
+    else 1
